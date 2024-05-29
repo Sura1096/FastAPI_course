00934f5e5b03eb5b6d5f62e6a2189268b56470a6
Sura <suri.murodova@gmail.com>
2024-05-29 15:00:17 +0300

    Add post-request to test endpoint

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+class Numbers(BaseModel):
+    num1: int
+    num2: int
+
+
 @app.get('/')
 async def root():
     return FileResponse('index.html')
+
+
+@app.post('/calculate/')
+async def calculate_sum(nums: Numbers):
+    result = nums.num1 + nums.num2
+    return {'result': result}
