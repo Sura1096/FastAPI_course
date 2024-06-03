09c31b0a00574c9fd4332579717b04e59feba9c5
Sura <suri.murodova@gmail.com>
2024-06-03 13:46:20 +0300

    Add endpoint that returns name, age and whether the user is an adult

--- lessons/second_chapter/users/main.py
+++ lessons/second_chapter/users/main.py
@@ -11,3 +11,10 @@ my_user: UserID = UserID(name='Sura', id=6)
 @app.get('/users')
 def get_user():
     return {'name': my_user.name, 'id': my_user.id}
+
+
+@app.post('/user')
+async def is_adult(user_data: UserAge):
+    return {'name': user_data.name,
+            'age': user_data.age,
+            'is_adult': user_data.age >= 18}
