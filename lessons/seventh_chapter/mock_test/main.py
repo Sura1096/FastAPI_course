275d62027cd5fa6c5473bfd75dfd5079e31b0433
Sura <suri.murodova@gmail.com>
2024-06-26 09:53:44 +0300

    Rename coroutine name for get anime data endpoint

--- lessons/seventh_chapter/mock_test/main.py
+++ lessons/seventh_chapter/mock_test/main.py
@@ -16,8 +16,22 @@ def get_anime(anime_id):
     return anime.get_anime_by_id(anime_id)
 
 
+@app.post("/create/")
+async def create_anime_item(anime_model: AnimeModel):
+    data = get_anime(anime_model.anime_id)
+    if data:
+        anime_data = get_parsed_data(data)
+        if anime_data["mal_id"] not in anime_db:
+            anime_db.update({anime_data["mal_id"]: anime_data})
+            return anime_data
+        else:
+            raise HTTPException(status_code=409, detail="Anime already exists")
+    else:
+        raise HTTPException(status_code=404, detail="Anime not found")
+
+
 @app.get("/data/{anime_id}", response_model=dict)
-async def get_anime(anime_id: int):
+async def get_anime_data(anime_id: int):
     data = get_anime(anime_id)
     if data:
         return get_parsed_data(data)
